12b82f9ff2bcd89acbf3d8e8093ebc71f84f6c34
tarunangrish <trn.angrish22@gmail.com>
2018-06-25 22:12:12 +0530

    Course contents updated

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -41,5 +41,7 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
 #Implementing Backward Elimination
-import statsmodel.formula.api as sm
-X = np.append()
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
